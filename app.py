7dee257e25befaa09853fc320901b0aa7197a97f
Yara Kyrychenko <70731529+yarakyrychenko@users.noreply.github.com>
2023-02-10 10:24:18 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from datetime import datetime
 from uuid import uuid4
 import seaborn as sns 
 import pandas as pd, numpy as np
-import streamlit_nested_layout
 
 
 st.set_page_config(
@@ -36,41 +35,32 @@ def format(option):
 st.session_state.one_columns_params = (.1, 3.2, .1)
 st.session_state.radio_columns_params = (4.5, 1)
 
-row0sep1, row0col1, row0sep2 = st.columns(st.session_state.one_columns_params)
-with row0sep1:
-    st.markdown("")
+st.markdown("<h1 style='text-align: center;'> 🧐 MIST 🧐 </h1>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center;'> Test your misinformation susceptibility </h2>", unsafe_allow_html=True)
 
-with row0col1:
-    st.markdown("<h1 style='text-align: center;'> 🧐 MIST 🧐 </h1>", unsafe_allow_html=True)
-    st.markdown("<h2 style='text-align: center;'> Test your misinformation susceptibility </h2>", unsafe_allow_html=True)
-
-    placeholder = st.empty()
-    with placeholder.container():
-        with st.expander("Consent", expanded=True):
-            st.markdown("""
+placeholder = st.empty()
+with placeholder.container():
+    with st.expander("Consent", expanded=True):
+        st.markdown("""
             By submitting the form below you agree to your data being used for research. 
             Your twitter handle will be stored in a private data base and will not be shared with anyone other than the researchers (unless extraordinary circumstances force us to). 
             You can ask for your data to be deleted by emailing us with an ID number you'll be issued after submitting the form. 
             """)
-            agree = st.checkbox("I understand and consent.")
+        agree = st.checkbox("I understand and consent.")
 
-    if agree:
-        placeholder.empty()
-        with st.expander("Consent", expanded=False):
-            st.markdown("""
+if agree:
+    placeholder.empty()
+    with st.expander("Consent", expanded=False):
+        st.markdown("""
             By submitting the form below you agree to your data being used for research. 
             Your twitter handle will be stored in a private data base and will not be shared with anyone other than the researchers (unless extraordinary circumstances force us to). 
             You can ask for your data to be deleted by emailing us with an ID number you'll be issued after submitting the form. 
             """)
-            st.markdown("You have consented.")
-     
-with row0sep2:
-    st.markdown("")   
-
+        st.markdown("You have consented.")
+      
 st.session_state.submitted = False
 st.session_state.disable = True 
 st.session_state.two_columns_params = (.1, 1.5, .2, 1.5, .1)
-
 
 if agree:
     import pymongo
@@ -79,14 +69,10 @@ if agree:
     #db = client.polarization
     #st.session_state.collection = db.app
 
-    formsep1, formcol1, formsep2 = st.columns(st.session_state.one_columns_params)
-    with formsep1:
-        st.markdown("")
 
-    with formcol1:
-        form_place = st.empty()
-        with form_place.container():
-            st.session_state.mist_items = [
+    form_place = st.empty()
+    with form_place.container():
+        st.session_state.mist_items = [
              "Government Officials Have Manipulated Stock Prices to Hide Scandals",
              "The Corporate Media Is Controlled by the Military-industrial Complex: The Major Oil Companies Own the Media and Control Their Agenda",
              "New Study: Left-Wingers Are More Likely to Lie to Get a Higher Salary",
@@ -108,58 +94,55 @@ if agree:
              "Global Warming Age Gap: Younger Americans Most Worried",
              "US Support for Legal Marijuana Steady in Past Year"
             ]
-            st.session_state.labels = ["Fake","Fake","Fake","Fake","Fake","Fake","Fake","Fake","Fake","Fake",
+        st.session_state.labels = ["Fake","Fake","Fake","Fake","Fake","Fake","Fake","Fake","Fake","Fake",
                                        "Real","Real","Real","Real","Real","Real","Real","Real","Real","Real"]
             
-            if "order" not in st.session_state:
-                st.session_state.order = ['', 'Real','Fake'] if np.random.random() <= .5 else ['','Fake','Real']
-            if "items_order" not in st.session_state:
-                st.session_state.items_order = np.arange(20)
-                np.random.shuffle(st.session_state.items_order)
-            st.session_state.answers = []
+        if "order" not in st.session_state:
+            st.session_state.order = ['', 'Real','Fake'] if np.random.random() <= .5 else ['','Fake','Real']
+        if "items_order" not in st.session_state:
+            st.session_state.items_order = np.arange(20)
+            np.random.shuffle(st.session_state.items_order)
+        st.session_state.answers = []
             
-            with st.expander("Form",expanded=True):
-                st.markdown("##### Please categorize the following news headlines as either 'Fake News' or 'Real News'.")
+        with st.expander("Form",expanded=True):
+            st.markdown("##### Please categorize the following news headlines as either 'Fake News' or 'Real News'.")
+            formprompt, formchoice = st.columns(st.session_state.radio_columns_params)
+            with formprompt:
+                st.markdown("")
+            with formchoice:
+                st.write(f"{st.session_state.order[1]}\t{st.session_state.order[2]}")
+                    
+            j =0
+            for i in st.session_state.items_order:
+                j+=1
                 formprompt, formchoice = st.columns(st.session_state.radio_columns_params)
                 with formprompt:
-                    st.markdown("")
+                    st.write("")
+                    st.markdown(st.session_state.mist_items[i])
                 with formchoice:
-                    st.write(f"{st.session_state.order[1]}\t{st.session_state.order[2]}")
-                    
-                j =0
-                for i in st.session_state.items_order:
-                    j+=1
-                    formprompt, formchoice = st.columns(st.session_state.radio_columns_params)
-                    with formprompt:
-                        st.write("")
-                        st.markdown(st.session_state.mist_items[i])
-                    with formchoice:
-                        st.session_state.answers.append(st.radio("", st.session_state.order, key = "q"+str(j+1), format_func=format, label_visibility="collapsed", horizontal=True))
+                    st.session_state.answers.append(st.radio("", st.session_state.order, key = "q"+str(j+1), format_func=format, label_visibility="collapsed", horizontal=True))
                      
-                st.session_state.disable = True if st.session_state.q20 == "" else False
+            st.session_state.disable = True if st.session_state.q20 == "" else False
  
                 #if st.session_state.disable:
                    # st.warning("Please fill out every field of the form to enable the submit button.")              
-                st.session_state.submitted = st.button("Submit", disabled=st.session_state.disable)
+            st.session_state.submitted = st.button("Submit", disabled=st.session_state.disable)
         
-        if  st.session_state.submitted:
+    if  st.session_state.submitted:
             #form_place.empty()
-            pass
+        pass
  
-        with st.expander("Form Submitted",expanded=True):
-            if st.session_state.submitted:
-                st.session_state.id = datetime.now().strftime('%Y%m-%d%H-%M-') + str(uuid4())
-                st.markdown(f"Thanks for submitting your answers! Your app ID is **{st.session_state.id}**. [Email me](mailto:yara@nyu.edu) with it if you want your answers deleted.") 
+    with st.expander("Form Submitted",expanded=True):
+        if st.session_state.submitted:
+            st.session_state.id = datetime.now().strftime('%Y%m-%d%H-%M-') + str(uuid4())
+            st.markdown(f"Thanks for submitting your answers! Your app ID is **{st.session_state.id}**. [Email me](mailto:yara@nyu.edu) with it if you want your answers deleted.") 
 
-                user_data = {
+            user_data = {
                             "id": st.session_state.id, 
                             "answers": st.session_state.answers, 
                             }
 
                 #st.session_state.collection.insert_one(user_data)        
-
-    with formsep2:
-        st.markdown("")
 
     if st.session_state.submitted:
         st.session_state.graded = []
