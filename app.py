51f8f6868de6bb572198c925cd0444319481024a
Yara Kyrychenko <70731529+yarakyrychenko@users.noreply.github.com>
2023-02-10 09:37:42 +0000

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,6 @@ st.markdown(
         """,
     unsafe_allow_html=True
 )
-st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 
 def format(option):
     return ""
@@ -121,16 +120,22 @@ if agree:
             with st.expander("Form",expanded=True):
                 st.markdown("##### Please categorize the following news headlines as either 'Fake News' or 'Real News'.")
                 
-                formprompt, formchoice = st.columns(st.session_state.radio_columns_params)
-                
+                for i in st.session_state.items_order:
+                    formprompt, formchoice = st.columns(st.session_state.radio_columns_params)
+                    with formprompt:
+                        st.markdown("")
+                        st.markdown(st.session_state.mist_items[i])
+                   with formchoice:
+                        st.session_state.answers.append(st.radio("", st.session_state.order, key = "q"+str(i+1), format_func=format, label_visibility="visible", horizontal=True))
+
                 #with formprompt:
                   #  st.markdown("                                                                        ")
                    # for i in st.session_state.items_order:
                       #  st.markdown(st.session_state.mist_items[i])
                 #with formchoice:
                 #st.markdown(f"{st.session_state.order[1]}         {st.session_state.order[2]}")
-                for i in st.session_state.items_order:
-                        st.session_state.answers.append(st.radio(st.session_state.mist_items[i], st.session_state.order, key = "q"+str(i+1), format_func=format, label_visibility="visible", horizontal=True))
+                #for i in st.session_state.items_order:
+                    #    st.session_state.answers.append(st.radio("", st.session_state.order, key = "q"+str(i+1), format_func=format, label_visibility="visible", horizontal=True))
                 
                      
                 st.session_state.disable = True if st.session_state.q20 == "" else False
