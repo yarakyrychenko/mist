22504a2d1da29f236dbb757bcee59b2b6ee0f7a8
Yara Kyrychenko <70731529+yarakyrychenko@users.noreply.github.com>
2023-02-10 11:13:22 +0000

    Update app.py

--- app.py
+++ app.py
@@ -181,11 +181,11 @@ if agree:
         st.markdown("")
         
         st.markdown("##### 📈 Your MIST results")
-        st.markdown(f"**Veracity Discernment: {st.session_state.score} out of 20** *(ability to accurately distinguish real news from fake news)*")
-        st.markdown(f"Real News Detection: {st.session_state.r} out of 10 *(ability to correctly identify real news)*")
-        st.markdown(f"Fake News Detection: {st.session_state.f} out of 10 *(ability to correctly identify fake news)*")
-        st.markdown(f"Distrust: {st.session_state.d} out of 10 *(negative judgment bias or being overly skeptical)*")
-        st.markdown(f"Naïvité: {st.session_state.n} out of 10 *(positive judgment bias or being overly gullible)*")
+        st.markdown(f"**Veracity Discernment: {st.session_state.score}/20** *(ability to accurately distinguish real news from fake news)*")
+        st.markdown(f"**Real News Detection: {st.session_state.r}/10** *(ability to correctly identify real news)*")
+        st.markdown(f"Fake News Detection: {st.session_state.f}/10** *(ability to correctly identify fake news)*")
+        st.markdown(f"Distrust: {st.session_state.d}/10** *(negative judgment bias or being overly skeptical)*")
+        st.markdown(f"Naïvité: {st.session_state.n}/10** *(positive judgment bias or being overly gullible)*")
 
         
         
