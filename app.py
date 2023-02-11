9906a18d44a4a89ce819f84544bc9970cf6fdc20
Yara Kyrychenko <70731529+yarakyrychenko@users.noreply.github.com>
2023-02-11 18:27:17 +0000

    Update app.py

--- app.py
+++ app.py
@@ -192,8 +192,7 @@ if agree or disagree:
             if "dem_submitted" not in st.session_state:
                 st.session_state.dem_submitted = False
 
-            demplaceholder = st.empty()
-            with demplaceholder.container():
+            with st.container():
                 with st.expander("Optional Questions", expanded=True):
                     st.markdown("##### Please answer a few more questions while we're calculating your score.") 
                     st.markdown("*Your answers to these questions are not taken into considerations in your MIST results.*")
@@ -203,12 +202,6 @@ if agree or disagree:
                     st.radio('What the highest level of education you completed?', ['', 'High School or Less', 'Some College', 'Higher Degree'])
                     st.radio('What is your political orientation?', ['', 'Extremely liberal', 'Liberal', 'Slightly liberal', 'Moderate', 'Slightly conservative', 'Conservative', 'Extremely conservative'])
                     st.session_state.dem_submitted = st.button("Submit",key="dem_sub")
-
-            if st.session_state.dem_submitted:
-                demplaceholder.empty()
-                with st.expander("Optional Questions", expanded=False):
-                    st.markdown("You submitted.")
-                    st.markdown("*Your answers to these questions are not taken into considerations in your MIST results.*")
 
             if st.session_state.dem_submitted:
 
