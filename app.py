541fda3886c2127ec2eaeafccecff4fd396b661a
Yara Kyrychenko <70731529+yarakyrychenko@users.noreply.github.com>
2023-02-11 17:03:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -106,17 +106,23 @@ if disagree:
 
         This privacy policy was updated on Feb 11, 2023.
         """)
-        
+
 if agree:
-    st.markdown("*Your answers to these questions are not taken into considerations when calculating your MIST results.*")
-    st.text_input('What is your Twitter handle?', key="twitter_handle")
-    st.text_input('What is your age?', key="age")
-    st.radio('What is your gender?', ['', 'Male', 'Female', 'Other'])
-    st.radio('What the highest level of education you completed?', ['', 'High School or Less', 'Some College', 'Higher Degree'])
-    st.radio('What is your political orientation?', ['', 'Extremely liberal', 'Liberal', 'Slightly liberal', 'Moderate', 'Slightly conservative', 'Conservative', 'Extremely conservative'])
+    demplaceholder = st.empty()
+    with demplaceholder.container():
+        with st.expander("Optional Questions", expanded=True):
+            st.markdown("*Your answers to these questions are not taken into considerations when calculating your MIST results.*")
+            st.text_input('What is your Twitter handle?', key="twitter_handle")
+            st.text_input('What is your age?', key="age")
+            st.radio('What is your gender?', ['', 'Male', 'Female', 'Other'])
+            st.radio('What the highest level of education you completed?', ['', 'High School or Less', 'Some College', 'Higher Degree'])
+            st.radio('What is your political orientation?', ['', 'Extremely liberal', 'Liberal', 'Slightly liberal', 'Moderate', 'Slightly conservative', 'Conservative', 'Extremely conservative'])
 
-    dem_submitted = st.button("Submit")
-
+            dem_submitted = st.button("Submit",key="dem_sub")
+    
+    if dem_submitted:
+        demplaceholder.empty()
+        
 st.session_state.submitted = False
 st.session_state.disable = True 
 
