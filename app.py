d6a55726b997cef43f61d17d75ca9dc427e60d22
Yara Kyrychenko <70731529+yarakyrychenko@users.noreply.github.com>
2023-05-15 20:43:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -252,7 +252,7 @@ if agree or disagree:
             else:
                 with st.expander("", expanded=True):
                     st.header("⚠️ You might be susceptible!")
-                    st.markdown(f'<h1 style="font-size: 2;"> You are less resilient to misinformation than **{100 - st.session_state.ustable[st.session_state.score]}%** of the US population! </h1>',unsafe_allow_html=True) 
+                    st.markdown(f'<p style="font-size: 4;"> You are less resilient to misinformation than **{100 - st.session_state.ustable[st.session_state.score]}%** of the US population! </p>',unsafe_allow_html=True) 
                     st.header(f"📈 Your MIST results: {st.session_state.score}/20")
                     st.markdown(f"**Veracity Discernment: {10*st.session_state.score_print}%** *(ability to accurately distinguish real news from fake news)*")
                     st.markdown(f"**Real News Detection: {10*st.session_state.r}%** *(ability to correctly identify real news)*")
